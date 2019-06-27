d881cc83935e5f61671897f83ae96c8f7c07ed0d
Joseph Davis <joseph.davis@hrblock.com>
2019-06-27 08:52:17 -0500

    Finish app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,86 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Card from 'react-bootstrap/Card';
+import Form from 'react-bootstrap/Form';
+import marked from 'marked';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 
+marked.setOptions({
+  breaks: true
+});
+
+const placeholder = `# Heading 1
+## Heading 2
+
+[Link to Google](https://www.google.com)
+
+Inline code: \`console.log('Inline code')\`
+
+\`\`\`javascript
+const s = 'Multiline code';
+console.log(s)
+\`\`\`
+
+List:
+* Item 1
+* Item 2
+* Item 3
+
+**Here's some bolded text**
+
+Blockquote:
+
+> Boy I really love quoting things.
+> It's one of my favorite pastimes!
+
+Image:
+
+![alt text](https://github.com/adam-p/markdown-here/raw/master/src/common/images/icon48.png "Logo")
+`;
+
 class App extends Component {
+  state = {
+    markdown: placeholder
+  };
+
+  onChange = e => {
+    this.setState({ markdown: e.target.value });
+  };
+
+  createMarkup = () => {
+    return { __html: marked(this.state.markdown) };
+  };
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <div className="m-5">
+          <div className="row">
+            <div className="col-md-6">
+              <h2>Markdown</h2>
+              <Form.Group>
+                <Form.Control
+                  id="editor"
+                  as="textarea"
+                  rows="20"
+                  onChange={this.onChange}
+                  value={this.state.markdown}
+                />
+              </Form.Group>
+            </div>
+            <div className="col-md-6">
+              <h2>Preview</h2>
+              <Card>
+                <Card.Body>
+                  <div
+                    id="preview"
+                    dangerouslySetInnerHTML={this.createMarkup()}
+                  />
+                </Card.Body>
+              </Card>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
